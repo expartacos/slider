479321d67e0abedbb2aed493af5c5a6255bf31f9
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-08 10:20:47 -0300

    Adicionando calculo do tamanho dos slider e os botões no centro

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
+let countSlider = document.querySelectorAll('.slider--item').length;
 
+document.querySelector('.slider--width').style.width = `calc(100vw * ${countSlider})`;
+document.querySelector('.controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
 
+console.log(document.querySelector('.slider').clientHeight);
